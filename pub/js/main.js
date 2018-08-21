938cc61df87d013ea2bd83da77fa8718ff9e2fd2
Takehiro Ikeda <t-ikeda@ruby-dev.jp>
2018-08-21 10:38:54 +0900

    extend html and js, css

--- pub/js/main.js
+++ pub/js/main.js
@@ -6,3 +6,19 @@ new Vue({
     }
   }
 })
+
+var app2 = new Vue({
+  el: '#app-2',
+  data: {
+    message: 'You loaded this page on ' + new Date().toLocaleDateString()
+  }
+})
+
+var app3 = new Vue({
+  el: '#app-3 #app-3-1',
+  components: {
+    'hoge002': {
+      template: '<p id="cl1">テスト</p>'
+    }
+  }
+})
