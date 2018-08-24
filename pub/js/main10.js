56efb2f9f14bd1eea4b6e7f95a21548d02437a35
Takehiro Ikeda <t-ikeda@ruby-dev.jp>
2018-08-24 17:59:41 +0900

    added app-15/app-16/app-17

---
+++ pub/js/main10.js
@@ -0,0 +1,21 @@
+// 他にイベントハンドラを使うものがないけど、passiveも設定できるというもの
+var app15 = new Vue({
+  el: '#app-15',
+  data: {
+    message: ""
+  }
+})
+
+var app16 = new Vue({
+  el: '#app-16',
+  data: {
+    picked: []
+  }
+})
+
+var app17 = new Vue({
+  el: '#app-17',
+  data: {
+    selected: ''
+  }
+})
